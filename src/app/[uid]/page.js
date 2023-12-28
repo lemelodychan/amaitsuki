40a6b499532aef31515afa55f6fe9feb1bd25379
Melody <elodie.dassance@gmail.com>
2023-12-28 19:34:06 +0900

    Update pages

--- src/app/[uid]/page.js
+++ src/app/[uid]/page.js
@@ -15,17 +15,25 @@ export async function generateMetadata({ params }) {
   };
 }
 
-  export default async function Page({ params }) {
-    const client = createClient();
-  
-    const casestudy = await client.getByUID("page", params.uid)
-  
-    return (
-      <div className={styles.main}>
-        <div className={styles.container}>
-          <PrismicRichText field={page.data.title} />
-          <SliceZone slices={page.data.slices} components={components} />
-        </div>
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("page");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
+
+export default async function Page({ params }) {
+  const client = createClient();
+  const page = await client.getByUID("page", params.uid)
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <PrismicRichText field={page.data.title} />
+        <SliceZone slices={page.data.slices} components={components} />
       </div>
-    );
-  }
+    </div>
+  );
+}
