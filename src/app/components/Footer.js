16f7e3589cdad4818bfbe21de0a2e4e08ca6eea2
Melody <elodie.dassance@gmail.com>
2024-11-30 01:34:05 +0900

    Footer styling

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -11,9 +11,16 @@ export default async function Footer() {
 
     return (
         <footer className={styles.Footer}>
-            <div className={styles.FooterWave}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#BD96FD" fill-opacity="1" d="M0,64L80,90.7C160,117,320,171,480,160C640,149,800,75,960,42.7C1120,11,1280,21,1360,26.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+            <div className={styles.FooterWave}> 
+                <svg width="1440" height="90" viewBox="0 0 1440 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_293_1287)">
+                    <path d="M0 21.3L60 16C120 10.3 240 0.3 360 0C480 0.3 600 10.3 720 32C840 53.3 960 85.3 1080 85.3C1200 85.3 1320 53.3 1380 37.3L1440 21.3V181.3H1380C1320 181.3 1200 181.3 1080 181.3C960 181.3 840 181.3 720 181.3C600 181.3 480 181.3 360 181.3C240 181.3 120 181.3 60 181.3H0V21.3Z" fill="#BD96FD"/>
+                    </g>
+                    <defs>
+                    <clipPath id="clip0_293_1287">
+                    <rect width="1440" height="90" fill="white"/>
+                    </clipPath>
+                    </defs>
                 </svg>
             </div>
             <div className={styles.FooterContainer}>
