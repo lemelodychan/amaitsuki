fdc2396a24759cb533220f3dd507cd4fff3c76e2
Melody <elodie.dassance@gmail.com>
2024-11-29 15:35:43 +0900

    Social menu styling desktop

--- src/slices/SocialMenu/index.js
+++ src/slices/SocialMenu/index.js
@@ -3,14 +3,28 @@
  * @typedef {import("@prismicio/react").SliceComponentProps<SocialMenuSlice>} SocialMenuProps
  * @param {SocialMenuProps}
  */
+
+import { PrismicNextLink } from "@prismicio/next";
+import { FaYoutube, FaXTwitter, FaInstagram, FaTiktok } from "react-icons/fa6";
+
 const SocialMenu = ({ slice }) => {
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for social_menu (variation: {slice.variation})
-      Slices
+      <PrismicNextLink field={slice.primary.youtube}>
+        <FaYoutube />
+      </PrismicNextLink>
+      <PrismicNextLink field={slice.primary.twitter}>
+        <FaXTwitter />
+      </PrismicNextLink>
+      <PrismicNextLink field={slice.primary.instagram}>
+        <FaInstagram />
+      </PrismicNextLink>
+      <PrismicNextLink field={slice.primary.tiktok}>
+        <FaTiktok />
+      </PrismicNextLink>
     </section>
   );
 };
